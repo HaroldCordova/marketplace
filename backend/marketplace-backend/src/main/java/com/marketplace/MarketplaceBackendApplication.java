e1e9cbbea4d210ef9152a508aa718808539f7515
HaroldCordova <Victor.62706827@gmail.com>
2025-10-12 22:48:15 -0500

    fix: agregar carpeta backend faltante

---
+++ backend/marketplace-backend/src/main/java/com/marketplace/MarketplaceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceBackendApplication.class, args);
+		System.out.println("🚀 Servidor backend Marketplace iniciado correctamente...");
+	}
+
+}
